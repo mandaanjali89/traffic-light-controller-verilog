67c1df68f0d88ef9fd14078a6989beda27e7a1d5
mandaanjali89 <mandaanjali89@gmail.com>
2026-08-08 14:08:04 +0530

    Update traffic_light_controller_tb.v

--- traffic_light_controller_tb.v
+++ traffic_light_controller_tb.v
@@ -1,1 +1,32 @@
+`timescale 1ns/1ps
 
+module traffic_light_tb;
+
+reg clk;
+reg reset;
+
+wire red;
+wire yellow;
+wire green;
+
+traffic_light uut (
+    .clk(clk),
+    .reset(reset),
+    .red(red),
+    .yellow(yellow),
+    .green(green)
+);
+
+always #5 clk = ~clk;
+
+initial
+begin
+    clk = 0;
+    reset = 1;
+
+    #10 reset = 0;
+
+    #100 $finish;
+end
+
+endmodule
